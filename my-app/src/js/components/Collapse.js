776e280fd8f3d87ae4adcb4ba1816af2bafee8f5
Marie-Hélène <mh.roussel@hotmail.fr>
2024-01-11 18:55:21 +0100

    collapse corrigé, juste un oublie d'accolade.

--- my-app/src/js/components/Collapse.js
+++ my-app/src/js/components/Collapse.js
@@ -1,42 +1,43 @@
 import React, { useState } from "react";
 import arrow from "../../asset/font/arrow_back_ios-24px 2.svg";
 
-const Collapse = ({ titleOk, textOk }) => {
+const Collapse = ({ titleKey, textKey }) => {
   // ça permet de déterminer si le texte s'affiche et qu'il est ouvert! il est par défaut en faux
-  const [isDevelop, setDevelop] = useState(false);
+  const [isExpanded, setExpanded] = useState(false);
 
   // c'est la fonction au moment ou on click sur la flèche retour
-  const handClick = () => {
-    setDevelop(!isDevelop);
+  const handleClick = () => {
+    setExpanded(!isExpanded);
   };
 
-  // la constante qui permet de vérifier que textOk ne soit pas vide
-  const bulletedList = Array.isArray(textOk) && textOk.length > 0;
+  // la constante qui permet de vérifier que textKey ne soit pas vide
+  const bulletedList = Array.isArray(textKey) && textKey.length > 0;
 
   // implémentation de la liste à puces
   const contentElement = bulletedList ? (
-    <ul className={'collapse${ isDevelop ? "-active" : "" }  '}>
-      {textOk.map((item, index) => (
+    <ul className={'collapse${ isExpanded ? "-active" : "" }  '}>
+      {textKey.map((item, index) => (
         <li key={index}>{item}</li>
       ))}
     </ul>
   ) : (
-    <p className={'collapse${ isDevelopp ? "-active" : ""}'}> {textOK} </p>
+    <p className={'collapse${ isExpanded ? "-active" : ""}'}> {textKey} </p>
   );
 
   return (
     <div className="description-container">
       <div className="main-deroulant">
-        <h2>{titleOk}</h2>
+        <h2>{titleKey}</h2>
         <img
           src={arrow}
           alt="flèche retour du site pour revenir en arrière"
-          onClick={handClick}
-          className={`arrowImg${isDevelop ? "-activ" : ""}`}
+          onClick={handleClick}
+          className={`arrowImg${isExpanded ? "-activ" : ""}`}
         />
       </div>
       {contentElement}
     </div>
   );
+  }
 
 export default Collapse;
