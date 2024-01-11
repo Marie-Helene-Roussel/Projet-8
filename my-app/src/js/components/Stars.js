1cac074f5bcf9ec94a5fabb68a9ce9a9843ea1b7
Marie-Hélène <mh.roussel@hotmail.fr>
2024-01-12 00:13:28 +0100

    quasi fini le projet casse pieds

--- my-app/src/js/components/Stars.js
+++ my-app/src/js/components/Stars.js
@@ -1,28 +1,22 @@
 import React from "react";
-import starFull from "../../img/star-active.png"
-import starEmpty from "../../img/star-inactive.png"
+import starFull from "../../img/star-active.png";
+import starEmpty from "../../img/star-inactive.png";
 
-const Stars = ({ rating }) =>{
-    // Le tableau d'étoile pour les notes de chaque logement
-    const stars = [];
-    for (let i=0; i < 5; i++) {
-        starEmpty.push(
-            <span key={i} className={ i < rating ? "star-filled" : "star-empty"} >
-                <img src={i < rating ? starFull : starEmpty} alt="étoile qui juge de la qualité du logement" />
+const Stars = ({ rating }) => {
+  // Le tableau d'étoile pour les notes de chaque logement
+  const stars = [];
+  for (let i = 0; i < 5; i++) {
+    starEmpty.push(
+      <span key={i} className={i < rating ? "star-filled" : "star-empty"}>
+        <img
+          src={i < rating ? starFull : starEmpty}
+          alt="étoile qui juge de la qualité du logement"
+        />
+      </span>
+    );
+  }
 
-            </span>
-        )
-    }
-
-    return <div className="stars-container">{stars}</div>
-
-}
-
-
-
-
-
-
-
+  return <div className="stars-container">{stars}</div>;
+};
 
 export default Stars;
