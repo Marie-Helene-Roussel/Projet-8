51e8bd6e325f73ce0aa6832b7561e219255c53b3
Marie-Hélène <mh.roussel@hotmail.fr>
2024-01-12 00:09:49 +0100

    composant d'étoiles faites

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './js/pages/Home';
-import Logement from './js/pages/Logement';
-import Error from './js/pages/Error';
-import About from './js/pages/About';
-
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./js/pages/Home";
+import Logement from "./js/pages/Logement";
+import Error from "./js/pages/Error";
+import About from "./js/pages/About";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/logement' element={<Logement />} />
-                <Route path='/error' element={<Error />}  />
-                <Route path='about' element={<About />} />
-
-
-
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/logement/:id" element={<Logement />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- my-app/src/js/components/Stars.js
+++ my-app/src/js/components/Stars.js
@@ -2,8 +2,20 @@ import React from "react";
 import starFull from "../../img/star-active.png"
 import starEmpty from "../../img/star-inactive.png"
 
-const Stars = () =>{
-    
+const Stars = ({ rating }) =>{
+    // Le tableau d'étoile pour les notes de chaque logement
+    const stars = [];
+    for (let i=0; i < 5; i++) {
+        starEmpty.push(
+            <span key={i} className={ i < rating ? "star-filled" : "star-empty"} >
+                <img src={i < rating ? starFull : starEmpty} alt="étoile qui juge de la qualité du logement" />
+
+            </span>
+        )
+    }
+
+    return <div className="stars-container">{stars}</div>
+
 }
 
 
